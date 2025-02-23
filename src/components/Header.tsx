1307767e747d327ad59c6bb965d4f110af657b47
PicoHBK <picohbk322@gmail.com>
2025-02-22 22:29:48 -0300

    add logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,12 @@
+import logo from "../assets/logo.png"
 function Header() {
+
   return (
     <div className="h-20 hidden w-full space-x-15 px-16 lg:flex">
-      <div className="flex items-center gap-2">
-        <div className="w-16 h-16 bg-white rounded-full"></div>
+      <div className="flex items-center select-none">
+        <div className="w-18 h-18">
+          <img src={logo} alt="logo" className="w-full h-full object-cover" />
+        </div>
         <h2 className="text-white font-bold text-2xl">SaltaGet</h2>
       </div>
       <nav className="flex gap-6 items-center">
